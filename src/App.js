2277805a7458bb16cc5cbefda123d704a84adbaf
tyler <taehyun9202@hotmail.com>
2021-05-05 15:08:22 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-red-500">
+      <h1 className="text-4xl">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
